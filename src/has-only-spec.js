dd2a48b2bd8326de5ab678cef2610bc86a633e6e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-03-12 22:48:52 -0400

    chore: initial

---
+++ src/has-only-spec.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/* eslint-env mocha */
+const hasOnly = require('.')
+
+describe('has-only', () => {
+  it('write this test', () => {
+    console.assert(hasOnly, 'should export something')
+  })
+})
